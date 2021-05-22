1ca1f25baaf96ff147ccb22f9938aeb1e0df3ae2
ginnygu <ginnygu@gmail.com>
2021-05-22 12:08:50 -0400

    FA-2: Fixed style and Added truncate to DeckTile component

--- packages/web-client/src/DeckTile/deck-tile.module.css.d.ts
+++ packages/web-client/src/DeckTile/deck-tile.module.css.d.ts
@@ -1,5 +1,7 @@
 declare const styles: {
-    readonly "deck-tile": string
+    readonly "deck-tile": string;
+    readonly "deck-tile-title": string;
+    readonly "deck-tile-description": string;
   }
   
   export default styles;
--- packages/web-client/src/DeckTile/deck-tile.tsx
+++ packages/web-client/src/DeckTile/deck-tile.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import styles from './deck-tile.module.css';
 export interface DeckTileProps {
 	data: DeckTileDataModel;
 }
@@ -13,9 +14,9 @@ export const DeckTile: FunctionComponent<DeckTileProps> = ({
 	data: { id, name, description },
 }) => {
 	return (
-		<pre className="deck-tile" key={id}>
-			{name}
-			{description}
+		<pre className={styles['deck-tile']} key={id}>
+			<p className={styles['deck-tile-title']}>{name}</p>
+			<p className={styles['deck-tile-description']}>{description}</p>
 		</pre>
 	);
 };
