9cda3862a0f20e2b3e2fa450819a7257466399b3
ginnygu <ginnygu@gmail.com>
2021-07-04 15:15:43 -0400

    FA-3: Create New Deck Functionality finished

--- packages/web-client/src/App/App.tsx
+++ packages/web-client/src/App/App.tsx
@@ -1,12 +1,16 @@
-
-import React, { useEffect, useState } from 'react';
-import { DeckList } from '../DeckList/deck-list';
-import Modal from '../Modal/Modal';
-import UseModal from '../Modal/UseModal';
-import styles from './App.module.css';
+import React, { useEffect, useState } from "react";
+import { DeckList } from "../DeckList/deck-list";
+import Modal from "../Modal/Modal";
+import UseModal from "../Modal/UseModal";
+import styles from "./App.module.css";
 
 function App() {
   const [deckList, setDeckList] = useState([]);
+  const [newDeckInput, setNewDeckInput] = useState({
+    id: "",
+    name: "",
+    description: "",
+  });
 
   const fetchDeckListData = async () => {
     const response = await fetch("http://localhost:3005/decks");
@@ -15,44 +19,49 @@ function App() {
     setDeckList(data);
   };
 
-  const [modalOpen, setModalOpen] = useState(false) 
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     fetchDeckListData();
-  }, []);
+  }, [deckList]);
 
   return (
     <div className="App">
       <div className={styles.header}>
         <h1 className={styles.header__logo}>
-        <a className={styles.header__logo} href="/">Flashly</a>  
+          <a className={styles.header__logo} href="/">
+            Flashly
+          </a>
         </h1>
         <div className={styles.navbar__container}>
-          
           <nav className={styles.navbar}>
-          <div className={styles.navbar__spaceA}>
-            <a className={styles.navbar__link} href="/decks">Decks</a>
-            <button 
-            onClick = {() => setModalOpen(true)} 
-            className={styles.navbar__button}
-            >
-              Create
-            </button>
-            <Modal modalOpen={modalOpen}>
-              <UseModal setModalOpen = {setModalOpen} />
-            </Modal>
-          </div>
-          
-          <div className={styles.navbar__spaceB}>
-          <button className={styles.navbar__button}>User Login</button>
-          </div>
+            <div className={styles.navbar__spaceA}>
+              <a className={styles.navbar__link} href="/decks">
+                Decks
+              </a>
+              <button
+                onClick={() => setModalOpen(true)}
+                className={styles.navbar__button}
+              >
+                Create
+              </button>
+              <Modal modalOpen={modalOpen}>
+                <UseModal
+                  setModalOpen={setModalOpen}
+                  setNewDeckInput={setNewDeckInput}
+                  newDeckInput={newDeckInput}
+                />
+              </Modal>
+            </div>
+
+            <div className={styles.navbar__spaceB}>
+              <button className={styles.navbar__button}>User Login</button>
+            </div>
           </nav>
         </div>
+      </div>
 
-      </div>
-      
-      <div id="modal-root">
-      </div>
+      <div id="modal-root"></div>
 
       <main>
         <div className="dashboard">
--- packages/web-client/src/Modal/UseModal.tsx
+++ packages/web-client/src/Modal/UseModal.tsx
@@ -1,43 +1,84 @@
-import React from 'react';
-import styles from './UseModal.module.css';
-
+import React from "react";
+import styles from "./UseModal.module.css";
 
 type UseModalProps = {
-    setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setNewDeckInput: Function;
+  newDeckInput: FormDataModel;
+};
+export interface FormDataModel {
+  id: string;
+  name: string;
+  description: string;
+}
+
+const UseModal: React.FC<UseModalProps> = ({
+  setModalOpen,
+  setNewDeckInput,
+  newDeckInput,
+}) => {
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const randomId = Math.floor(Math.random() * 10000).toString();
+    setNewDeckInput({ ...newDeckInput, [name]: value, id: randomId });
+  };
+  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3005/decks", {
+        method: "POST",
+        body: JSON.stringify(newDeckInput),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      setNewDeckInput({ name: "", description: "", id: "" });
+      setModalOpen(false);
+    } catch (error) {}
+  };
+  return (
+    <div className={styles.modal__background}>
+      <div className={styles.modal__contentContainer}>
+        <div className={styles.modal__content}>
+          <form
+            className={styles.modal__form}
+            onSubmit={(e) => handleOnSubmit(e)}
+          >
+            <label>
+              <input
+                className={styles.modal__title}
+                name="name"
+                placeholder="Title"
+                value={newDeckInput.name}
+                onChange={(e) => {
+                  handleOnChange(e);
+                }}
+              />
+            </label>
+
+            <label>
+              <input
+                className={styles.modal__description}
+                name="description"
+                placeholder="Description"
+                value={newDeckInput.description}
+                onChange={(e) => {
+                  handleOnChange(e);
+                }}
+              />
+            </label>
+
+            <div className={styles.modal__buttons}>
+              <button>Add</button>
+              <button type="button" onClick={() => setModalOpen(false)}>
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-const UseModal: React.FC<UseModalProps> = ({ setModalOpen }) => (
-    <div className={styles.modal__background}>
-        <div className={styles.modal__contentContainer}>
-            <div className={styles.modal__content}>
-                <form className={styles.modal__form}>
-
-                    
-                        <label>
-                            <input className={styles.modal__title} name="title" placeholder="Title"
-                            />
-                        </label>
-
-                        <label>
-
-                            <input className={styles.modal__description} name="description" placeholder="Description" />
-                        </label>
-
-
-                
-                    <div className={styles.modal__buttons}>
-                        <button>Add</button>
-                        <button
-                            type='button'
-                            onClick={() => setModalOpen(false)}
-                        >
-                            Cancel
-                </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-)
-
-export default UseModal
+export default UseModal;
